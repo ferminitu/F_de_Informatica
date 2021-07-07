740a1e2749eb5b2bc7278a1534237c334a3ba029
ferminitu <ferminliturriaga@gmail.com>
2021-07-07 10:59:49 -0400

    avance tps

--- F. de Informática/Python avanzado/Ciencia de Datos/2dointento.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-datos = pd.read_csv('https://raw.githubusercontent.com/AJVelezRueda/Fundamentos_de_informatica/master/Ciencia_de_datos/practicos/recursos/Datos.csv')
-print(datos)
